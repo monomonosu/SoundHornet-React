a2dc2bdc6e6215219677fe025bba051705642828
monomonosu <pojinyan@yahoo.co.jp>
2022-05-17 01:50:31 +0900

    MaterialUIIconインストール。ツールバー追加。

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MButton from '@mui/material/Button';
+import { AppBar, Toolbar, IconButton, Typography, Button, Box, Grid } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from "react-router-dom";
 
 type Token = {
@@ -11,7 +12,24 @@ type Token = {
 function App() {
   return (
     <div className="App">
-      <MButton variant="contained">ボタン</MButton>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar style={{ backgroundColor: "#0A0A0A" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              SoundHornet
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <nav>
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
