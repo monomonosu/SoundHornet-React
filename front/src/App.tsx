971c3b89efd21df1a700b87c51da1b9aa0448a3d
monomonosu <pojinyan@yahoo.co.jp>
2022-05-16 22:39:42 +0900

    不用箇所削除

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -11,25 +11,11 @@ type Token = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MButton variant="contained">ボタン</MButton>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <nav>
-          <Link to="/invoices">Invoices</Link> |{" "}
-          <Link to="/expenses">Expenses</Link>
-        </nav>
-      </header>
+      <MButton variant="contained">ボタン</MButton>
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
