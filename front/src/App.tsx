92636c02f6cbfe242de7a764433fcbacf6393b23
monomonosu <pojinyan@yahoo.co.jp>
2022-05-21 21:29:31 +0900

    テーブルを表示してみる

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AppBar, Toolbar, IconButton, Typography, Button, Box, Grid } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Button, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from "react-router-dom";
+// import axios from "axios"
 
-type Token = {
-  token: string;
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
 }
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+// function getMusics(){
+//   const musics = axios.get("http://10.231.136.165:8080/test",{
+//   }).then(function(res) {
+//     console.log(res.data);
+//     alert(res.data.result);
+//     return res.data;
+//   });
+// }
+
 function App() {
   return (
     <div className="App">
@@ -30,6 +55,41 @@ function App() {
           </Toolbar>
         </AppBar>
       </Box>
+
+      <Box>
+        <p>ここにジャケ画</p>
+      </Box>
+
+      <TableContainer component={Paper} style={{ backgroundColor: "#0E0E0E" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell style={{color:"#FFFFFF"}}>Dessert (100g serving)</TableCell>
+              <TableCell style={{color:"#FFFFFF"}} align="right">Calories</TableCell>
+              <TableCell style={{color:"#FFFFFF"}} align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell style={{color:"#FFFFFF"}} align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell style={{color:"#FFFFFF"}} align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
       <nav>
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
