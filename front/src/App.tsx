9e7706e2b6d70094059416eb5a92c618a703a6d3
monomonosu <pojinyan@yahoo.co.jp>
2022-05-22 18:43:21 +0900

    API取得データのテーブル出力テスト

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,40 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { AppBar, Toolbar, IconButton, Typography, Button, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from "react-router-dom";
 import axios from "axios"
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function getMusics() {
-  const musics = axios.get("http://localhost:8080/test", {
-  }).then(function (response) {
-    console.log(response.data);
-    return response.data;
-  });
+type Music = {
+  id: number;
+  musicName: string;
+  artist: string;
+  album: string;
+  genre: string;
+  fileSize: string;
 }
 
 function App() {
-  getMusics();
+  const [musics, setMusics] = useState<Music[]>([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/test")
+      .then((response) => {
+        console.log(response.data);
+        setMusics(response.data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
@@ -68,26 +59,26 @@ function App() {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell style={{ color: "#FFFFFF" }}>Dessert (100g serving)</TableCell>
-                  <TableCell style={{ color: "#FFFFFF" }} align="right">Calories</TableCell>
-                  <TableCell style={{ color: "#FFFFFF" }} align="right">Fat&nbsp;(g)</TableCell>
-                  <TableCell style={{ color: "#FFFFFF" }} align="right">Carbs&nbsp;(g)</TableCell>
-                  <TableCell style={{ color: "#FFFFFF" }} align="right">Protein&nbsp;(g)</TableCell>
+                  <TableCell style={{ color: "#FFFFFF" }}>MusicName</TableCell>
+                  <TableCell style={{ color: "#FFFFFF" }}>Artist</TableCell>
+                  <TableCell style={{ color: "#FFFFFF" }}>Album</TableCell>
+                  <TableCell style={{ color: "#FFFFFF" }}>Genre</TableCell>
+                  <TableCell style={{ color: "#FFFFFF" }} align="right">FileSize</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row) => (
+                {musics.map((music) => (
                   <TableRow
-                    key={row.name}
+                    key={music.musicName}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
-                    <TableCell component="th" scope="row">
-                      {row.name}
+                    <TableCell style={{ color: "#FFFFFF" }} component="th" scope="row">
+                      {music.musicName}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell style={{ color: "#FFFFFF" }}>{music.artist}</TableCell>
+                    <TableCell style={{ color: "#FFFFFF" }}>{music.album}</TableCell>
+                    <TableCell style={{ color: "#FFFFFF" }}>{music.genre}</TableCell>
+                    <TableCell style={{ color: "#FFFFFF" }} align="right">{music.fileSize}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
