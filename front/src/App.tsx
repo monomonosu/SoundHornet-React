63c761dc2799e2941d3850a9e49d4434fab13dfc
monomonosu <pojinyan@yahoo.co.jp>
2022-05-25 21:40:34 +0900

    ヘッダーのボタンにpopper適用

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import {
-  AppBar, Toolbar, IconButton, Typography, Button, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Drawer, List
+  AppBar, Toolbar, IconButton, Typography, Button, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Drawer, Popper
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -27,6 +27,7 @@ type Music = {
 }
 
 function App() {
+  // メソッド
   function PlaySound(music: Music) {
     const filepath = 'static/musics/' + music.musicName + '.mp3';
     console.log(music);
@@ -39,8 +40,39 @@ function App() {
   const drawerOpen = () => {
     setIsOpenDrawer(!isOpenDrawer);
   }
+  const openPopper = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+    setIsOpenPopper(true);
+  }
+  const closePopper = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+    setIsOpenPopper(false);
+  }
+  const openPopper2 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl2(event.currentTarget);
+    setIsOpenPopper2(true);
+  }
+  const closePopper2 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl2(event.currentTarget);
+    setIsOpenPopper2(false);
+  }
+  const openPopper3 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl3(event.currentTarget);
+    setIsOpenPopper3(true);
+  }
+  const closePopper3 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl3(event.currentTarget);
+    setIsOpenPopper3(false);
+  }
+  // ステート
   const [musics, setMusics] = useState<Music[]>([]);
-  const [isOpenDrawer, setIsOpenDrawer] = useState(false)
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [isOpenPopper, setIsOpenPopper] = useState(false);
+  const [isOpenPopper2, setIsOpenPopper2] = useState(false);
+  const [isOpenPopper3, setIsOpenPopper3] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+  const [anchorEl3, setAnchorEl3] = useState<null | HTMLElement>(null);
   useEffect(() => {
     axios.get("http://localhost:8080/test")
       .then((response) => {
@@ -89,17 +121,32 @@ function App() {
               SoundHornet
             </Typography>
             <Link to="/download">
-              <Button>
+              <Button onMouseEnter={openPopper} onMouseLeave={closePopper}>
                 <YouTubeIcon fontSize="large" />
+                <Popper open={isOpenPopper} anchorEl={anchorEl}>
+                  <Paper>
+                    <Typography sx={{ p: 2 }}>Download from Youtube</Typography>
+                  </Paper>
+                </Popper>
               </Button>
             </Link>
             <Link to="/import">
-              <Button>
+              <Button onMouseEnter={openPopper2} onMouseLeave={closePopper2}>
                 <BackupIcon fontSize="large" />
+                <Popper open={isOpenPopper2} anchorEl={anchorEl2}>
+                  <Paper>
+                    <Typography sx={{ p: 2 }}>Import</Typography>
+                  </Paper>
+                </Popper>
               </Button>
             </Link>
-            <Button>
+            <Button onMouseEnter={openPopper3} onMouseLeave={closePopper3}>
               <SettingsIcon fontSize="large" />
+              <Popper open={isOpenPopper3} anchorEl={anchorEl3}>
+                <Paper>
+                  <Typography sx={{ p: 2 }}>Setting</Typography>
+                </Paper>
+              </Popper>
             </Button>
           </Toolbar>
         </AppBar>
