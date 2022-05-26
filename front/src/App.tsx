f1ba20180f2dcc6cef4d48533997bc9e4a4ec08a
monomonosu <pojinyan@yahoo.co.jp>
2022-05-26 19:22:48 +0900

    トップページにHeaderコンポーネント適用

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,17 +2,9 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import {
-  AppBar, Toolbar, IconButton, Typography, Button, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Drawer, Popper
+  Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
 } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import BackupIcon from '@mui/icons-material/Backup';
-import SettingsIcon from '@mui/icons-material/Settings';
-import BoyIcon from '@mui/icons-material/Boy';
-import FilterIcon from '@mui/icons-material/Filter';
-import SellIcon from '@mui/icons-material/Sell';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Link } from "react-router-dom";
+import Header from './component/Header';
 import axios from "axios"
 import { Howl, Howler } from 'howler';
 
@@ -37,42 +29,8 @@ function App() {
     });
     sound.play();
   }
-  const drawerOpen = () => {
-    setIsOpenDrawer(!isOpenDrawer);
-  }
-  const openPopper = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-    setIsOpenPopper(true);
-  }
-  const closePopper = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-    setIsOpenPopper(false);
-  }
-  const openPopper2 = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl2(event.currentTarget);
-    setIsOpenPopper2(true);
-  }
-  const closePopper2 = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl2(event.currentTarget);
-    setIsOpenPopper2(false);
-  }
-  const openPopper3 = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl3(event.currentTarget);
-    setIsOpenPopper3(true);
-  }
-  const closePopper3 = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl3(event.currentTarget);
-    setIsOpenPopper3(false);
-  }
   // ステート
   const [musics, setMusics] = useState<Music[]>([]);
-  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
-  const [isOpenPopper, setIsOpenPopper] = useState(false);
-  const [isOpenPopper2, setIsOpenPopper2] = useState(false);
-  const [isOpenPopper3, setIsOpenPopper3] = useState(false);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
-  const [anchorEl3, setAnchorEl3] = useState<null | HTMLElement>(null);
   useEffect(() => {
     axios.get("http://localhost:8080/test")
       .then((response) => {
@@ -83,76 +41,8 @@ function App() {
 
   return (
     <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar style={{ backgroundColor: "#161B22" }}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={drawerOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Drawer
-              anchor="left"
-              open={isOpenDrawer}
-              onClose={drawerOpen}
-            >
-              <Box style={{ backgroundColor: "#161B22", height: '100vh' }}>
-                <Box style={{ height: '9vh' }}></Box>
-                <Grid container>
-                  <Button><BoyIcon />　Artist</Button>
-                </Grid>
-                <Grid container>
-                  <Button><FilterIcon />　Album</Button>
-                </Grid>
-                <Grid container>
-                  <Button><SellIcon />　Genre</Button>
-                </Grid>
-                <Grid container>
-                  <Button><FavoriteIcon />　Favorite</Button>
-                </Grid>
-              </Box>
-            </Drawer>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              SoundHornet
-            </Typography>
-            <Link to="/download">
-              <Button onMouseEnter={openPopper} onMouseLeave={closePopper}>
-                <YouTubeIcon fontSize="large" />
-                <Popper open={isOpenPopper} anchorEl={anchorEl}>
-                  <Paper>
-                    <Typography sx={{ p: 2 }}>Download from Youtube</Typography>
-                  </Paper>
-                </Popper>
-              </Button>
-            </Link>
-            <Link to="/import">
-              <Button onMouseEnter={openPopper2} onMouseLeave={closePopper2}>
-                <BackupIcon fontSize="large" />
-                <Popper open={isOpenPopper2} anchorEl={anchorEl2}>
-                  <Paper>
-                    <Typography sx={{ p: 2 }}>Import</Typography>
-                  </Paper>
-                </Popper>
-              </Button>
-            </Link>
-            <Link to="/setting">
-              <Button onMouseEnter={openPopper3} onMouseLeave={closePopper3}>
-                <SettingsIcon fontSize="large" />
-                <Popper open={isOpenPopper3} anchorEl={anchorEl3}>
-                  <Paper>
-                    <Typography sx={{ p: 2 }}>Setting</Typography>
-                  </Paper>
-                </Popper>
-              </Button>
-            </Link>
-          </Toolbar>
-        </AppBar>
-      </Box>
+
+      <Header></Header>
 
       <Grid container>
         <Grid item xs>
