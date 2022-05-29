d9951d2c2c64e9cf3fd04c674a70ae97e2853256
monomonosu <pojinyan@yahoo.co.jp>
2022-05-29 17:14:06 +0900

    materialUIのtextfieldをテスト的に使ってみる。

--- front/src/DownloadPage.tsx
+++ front/src/DownloadPage.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Grid } from "@mui/material";
 import Header from "./component/Header";
+import { Box, Button, TextField } from "@mui/material";
 
 const DownloadPage = () => {
+    const [url, setUrl] = useState('')
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(() => e.target.value)
+    }
     return (
         <div>
             <Header></Header>
@@ -9,6 +15,24 @@ const DownloadPage = () => {
                 <Grid item xs></Grid>
                 <Grid item xs={11}>
                     <h1 style={{ color: "white" }}>Download</h1>
+                    <h2 style={{ color: "white" }}>InputURL</h2>
+                    <Box
+                        component="form"
+                        sx={{
+                            m: 1,
+                            width: 1500,
+                            maxWidth: '100%',
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        style={{ backgroundColor: "white" }}
+                    >
+                        <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined"
+                            value={url}
+                            onChange={handleChange}
+                        />
+                    </Box>
+                    <Button onClick={() => alert(url)}>submit</Button>
                 </Grid>
                 <Grid item xs></Grid>
             </Grid>
