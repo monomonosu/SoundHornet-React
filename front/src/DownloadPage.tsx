474196c49690984484c76eb4afc5eac3f366d90e
monomonosu <pojinyan@yahoo.co.jp>
2022-05-29 19:02:06 +0900

    ThemeProviderでデフォルトテーマの変更。入力欄に適用。

--- front/src/DownloadPage.tsx
+++ front/src/DownloadPage.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
-import { Grid } from "@mui/material";
 import Header from "./component/Header";
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Grid, createTheme, ThemeProvider } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const DownloadPage = () => {
     const [url, setUrl] = useState('')
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUrl(() => e.target.value)
     }
+    // テーマ変更
+    const theme = createTheme({
+        palette: {
+            secondary: {
+                main: grey[50]
+            }
+        }
+    });
     return (
         <div>
             <Header></Header>
@@ -19,18 +27,35 @@ const DownloadPage = () => {
                     <Box
                         component="form"
                         sx={{
-                            m: 1,
                             width: 1500,
                             maxWidth: '100%',
                         }}
                         noValidate
                         autoComplete="off"
-                        style={{ backgroundColor: "white" }}
                     >
-                        <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined"
-                            value={url}
-                            onChange={handleChange}
-                        />
+                        <ThemeProvider theme={theme}>
+                            <TextField fullWidth id="youTubeUrl" label="YouTubeURL" variant="outlined" color="secondary"
+                                value={url}
+                                onChange={handleChange}
+                                sx={{
+                                    '& .MuiInputBase-input': {
+                                        color: '#FFFFFF',    // 入力文字の色
+                                    },
+                                    '& label': {
+                                        color: '#FFFFFF', // 通常時のラベル色
+                                        '&:focusVisible': { color: '#FFFFFF' }
+                                    },
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: '#DDDDDD',    // 通常時のボーダー色(アウトライン)
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: '#FFFFFF',    // ホバー時のボーダー色(アウトライン)
+                                        },
+                                    },
+                                }}
+                            />
+                        </ThemeProvider>
                     </Box>
                     <Button onClick={() => alert(url)}>submit</Button>
                 </Grid>
