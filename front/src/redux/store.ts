ccd9d96fcce3931ab3e76c745f76e9376c02f872
monomonosu <pojinyan@yahoo.co.jp>
2022-07-27 19:59:53 +0900

    currentSeekステート管理をRecoilからReduxに変更

--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import isLoopReducer from '../redux/isLoopSlice'
 import playingIdReducer from '../redux/playingIdSlice'
 import volumeReducer from '../redux/volumeSlice';
+import currentSeekReducer from '../redux/currentSeekSlice'
 
 export const store = configureStore({
     reducer: {
         isLooper: isLoopReducer,
         playingId: playingIdReducer,
         volume: volumeReducer,
+        currentSeeker: currentSeekReducer
     },
 });
