7285290e67af9ea950d56e7ce67b4d1042940208
monomonosu <pojinyan@yahoo.co.jp>
2022-08-04 03:00:56 +0900

    Album新規作成モーダル作成

--- front/src/AlbumPage.tsx
+++ front/src/AlbumPage.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+// component
 import Header from './component/Header';
 import {
-    Box, Card, CardContent, CardMedia, IconButton, Typography, Grid, Button
+    Box, Card, CardContent, CardMedia, IconButton, Typography, Grid, Button, Modal, TextField, MenuItem, Backdrop, Snackbar, Alert, CircularProgress
 } from '@mui/material'
+// icons
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
+// types
+import { Album } from './types/albums';
+// redux
+import { useDispatch } from 'react-redux';
 
 export default function AlbumPage() {
     const theme = useTheme();
@@ -21,7 +30,7 @@ export default function AlbumPage() {
             <Grid container justifyContent="flex-end">
                 <Grid item xs={1}></Grid>
                 <Grid>
-                    <Button variant="contained" size='large' style={{ textTransform: "none" }}>CreateAlbum</Button>
+                    <CreateModal />
                 </Grid>
                 <Grid item xs={1}></Grid>
             </Grid>
@@ -137,5 +146,93 @@ export default function AlbumPage() {
                 <Grid item xs={1}></Grid>
             </Grid>
         </div >
+    );
+}
+
+export const CreateModal = () => {
+    const dispatch = useDispatch();
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isProgress, setIsProgress] = useState(false);
+    const [isSnackOpen, setIsSnackOpen] = useState(false);
+    const [albums, setAlbums] = useState<Album[]>([]);
+    const modalOpen = () => setIsOpenModal(true);
+    const modalClose = () => setIsOpenModal(false);
+    const { register, handleSubmit } = useForm<Album>();
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '80vw',
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const onSubmit: SubmitHandler<Album> = async (data) => {
+        setIsProgress(true);
+        await axios.put('/music/', data)
+            .then((response) => {
+                console.log(response);
+            })
+        setIsProgress(false);
+        setIsOpenModal(false);
+        setIsSnackOpen(true);
+    }
+
+    const snackbarClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setIsSnackOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="contained" size='large' style={{ textTransform: "none" }} onClick={modalOpen}>CreateAlbum</Button>
+            <Modal
+                open={isOpenModal}
+                onClose={modalClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        CreateAlbum
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{
+                        mt: 2, '& .MuiTextField-root': { m: 1 },
+                    }}>
+                        <div>
+                            <TextField style={{ width: '25ch' }} label="albumName" type="text" {...register('albumName')} variant="standard" />
+                        </div>
+                        <div>
+                            <Button
+                                sx={{ mt: 2 }}
+                                color="primary"
+                                variant="contained"
+                                size="large"
+                                onClick={handleSubmit(onSubmit)}
+                            >
+                                submit
+                            </Button>
+                        </div>
+                    </Typography>
+                </Box>
+            </Modal>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: 1301 }}
+                open={isProgress}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <Snackbar open={isSnackOpen} autoHideDuration={2000} onClose={snackbarClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert onClose={snackbarClose} severity="success" variant="filled" sx={{ width: '100%' }}>
+                    Save successfully!
+                </Alert>
+            </Snackbar>
+        </div>
     );
 }
